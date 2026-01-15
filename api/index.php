15acff5fb8bbb71aa3d2dcc75c4e1f0dde664473
Louka <louka.avenia@gmail.com>
2026-01-15 16:15:12 +0100

    vercel push

--- api/index.php
+++ api/index.php
@@ -4,8 +4,6 @@ ini_set('session.save_handler', 'files');
 ini_set('session.save_path', '/tmp');
 session_start();
 
-session_start();
-
 if (isset($_SESSION['user'])) {
     header('Location: dashboard.php');
 } else {
